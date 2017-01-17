24da216296251fed43df139b48077e255d0b2ec7
Jack Linton <jacklinton@mac.com>
2017-01-17 08:54:24 -0500

    Fixed the position of the hourhand before noon with function checkTheHour.

--- clock.js
+++ clock.js
@@ -3,8 +3,17 @@ var nowHours = nowTime.getHours()
 var nowMinutes = nowTime.getMinutes()
 var nowSeconds = nowTime.getSeconds()
 var secondHandAngle = 6 * nowSeconds + 180
-var hourHandAngle = ((Math.abs(12-nowHours))*(360/12)) + 180 + (nowMinutes/2)
+var hourHandAngle = checkTheHour()
 var minuteHandAngle = 6 * nowMinutes + 180
+
+
+function checkTheHour() {
+	if (nowHours > 12) {
+		return(((Math.abs(12-nowHours))*(360/12)) + 180 + (nowMinutes/2))
+	} else {
+		return(((nowHours)*(360/12)) + 180 + (nowMinutes/2))}
+}
+
 
 
 window.setInterval(function(){
